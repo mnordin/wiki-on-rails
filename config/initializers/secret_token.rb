c33f69d49a0101b527ff35ccabebd79e87758a7f
Markus Nordin <markus.nordin@gmail.com>
2010-11-04 10:05:12 +0100

    First push, ignoring the database yml but providing a default file. COPY it and edit your own settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiOnRails::Application.config.secret_token = '9589da12476f909c02e22ada7f08830e8f1663a9dcbd14a6402c4f239dd0a112c2ce9f991c958f17eb2c166fd3843df30cdbf36d20f91f122202c7be653b074b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WikiOnRails::Application.config.session_store :cookie_store, :key => '_wiki-on-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# WikiOnRails::Application.config.session_store :active_record_store
