918fd923c73981bdb527c1c10d3dced80a02d6fa
Markus Nordin <markus.nordin@gmail.com>
2010-11-04 11:47:29 +0100

    Article has been stripped of XML actions and added fields for body and title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,33 +3,18 @@ class ArticlesController < ApplicationController
   # GET /articles.xml
   def index
     @articles = Article.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @articles }
-    end
   end
 
   # GET /articles/1
   # GET /articles/1.xml
   def show
     @article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @article }
-    end
   end
 
   # GET /articles/new
   # GET /articles/new.xml
   def new
     @article = Article.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @article }
-    end
   end
 
   # GET /articles/1/edit
@@ -42,14 +27,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to(@article, :notice => 'Article was successfully created.') }
-        format.xml  { render :xml => @article, :status => :created, :location => @article }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @article.errors, :status => :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to(@article, :notice => 'Article was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
@@ -58,14 +39,10 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
 
-    respond_to do |format|
-      if @article.update_attributes(params[:article])
-        format.html { redirect_to(@article, :notice => 'Article was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @article.errors, :status => :unprocessable_entity }
-      end
+    if @article.update_attributes(params[:article])
+      redirect_to(@article, :notice => 'Article was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
@@ -75,9 +52,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(articles_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(articles_url)
   end
 end
