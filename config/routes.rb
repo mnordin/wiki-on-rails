905e7ae144b181784926826608ebf75ccc247d8e
Markus Nordin <markus.nordin@gmail.com>
2010-11-08 16:40:25 +0100

    Edited the model to_params so articles now support title pretty urls

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,8 @@ WikiOnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # root :to => "articles#show#start"
+  root :to => "articles#show"
 
   # See how all your routes lay out with "rake routes"
 
