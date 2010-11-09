75ad5f8902b8105533640dab79270b74a75d4eb8
anleon <alexander@leonov.se>
2010-11-09 14:31:17 +0100

    merge wikitranslate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
     #body.gsub!("\n", "<br/>")
     #body.gsub!("\*", "!")
     
-    body.gsub!(Regexp.new("===(.+)==="), "<h2>\1</h2>")
-    body.gsub!(Regexp.new("==(.+)=="), "<h3>\1</h3>")
-    body.gsub!(Regexp.new("\n\-\b(.+)", "<li>\1</li>"))
-    body.gsub!(Regexp.new("\[\[(\w+)\]\]"), "<a href=\"/mongoose-wiki/article/view/\1\">\1</a>")
-    body
+    body.gsub!(Regexp.new("===(.+)==="), '/<h2>\1</h2>/')
+    body.gsub!(Regexp.new("==(.+)=="), '<h3>\1</h3>')
+    body.gsub!(Regexp.new("\n\\*\s(.+)"), '<li>\1</li>')
+    body.gsub!(Regexp.new("\[\[(\w+)\]\]"), '<a href="/mongoose-wiki/article/view/\1">\1</a>')
+    
   end
 
 end
